5d5e0793468026afc4833f61c4b0f3e64ba59860
dhulipalasrikanth98 <dhulipalasrikanth98@outlook.com>
2024-10-02 00:36:35 +0530

    added the jpa support with mysql connection
    now we can save the data to the databse

--- src/main/java/com/springlearning/todoApplication/security/TodoSecurity.java
+++ src/main/java/com/springlearning/todoApplication/security/TodoSecurity.java
@@ -1,18 +1,20 @@
 package com.springlearning.todoApplication.security;
 
 import com.springlearning.todoApplication.entity.Todo;
-
+import static org.springframework.security.config.Customizer.withDefaults;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.stereotype.Service;
 
 @Configuration
@@ -36,5 +38,13 @@ public class TodoSecurity {
 	@Bean 
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests(auth->auth.anyRequest().authenticated());
+	    http.formLogin(withDefaults());
+	    http.csrf().disable();
+	    http.headers().frameOptions().disable();
+		return http.build();
 	}
 }
--- src/main/java/com/springlearning/todoApplication/service/TodoService.java
+++ src/main/java/com/springlearning/todoApplication/service/TodoService.java
@@ -1,64 +1,48 @@
 package com.springlearning.todoApplication.service;
 
 import com.springlearning.todoApplication.entity.Todo;
+import com.springlearning.todoApplication.service.repo.TodoRepository;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TodoService {
     private static List<Todo> todoList = new ArrayList<>();
-
- static {
-      todoList.add(new Todo(1,"srikanth","Learn Spring Boot", LocalDate.now().plusYears(1),false));
-      todoList.add(new Todo(2,"rajesh","Learn AWS", LocalDate.now().plusYears(1),false));
-      todoList.add(new Todo(3,"ravi","Learn Docker", LocalDate.now().plusYears(1),false));
-      todoList.add(new Todo(4,"kiran","Learn Jenkins", LocalDate.now().plusYears(1),false));
-
+    private TodoRepository todoRepository;
+    @Autowired
+    public TodoService(TodoRepository todoRepository) {
+    	this.todoRepository = todoRepository;
     }
 
-    public  void addToTodoList(String username,String description, LocalDate target, boolean isDone) {
-        int last = todoList.size() + 1;
-        todoList.add(new Todo(last,username,description,target,isDone));
+    public  void addToTodoList(String username,Todo todo) {
+        todo.setUsername(username);
+        todoRepository.save(todo);
     }
 
     public  List<Todo> getAllTodo() {
-        return todoList;
+        return todoRepository.findAll();
     }
     public  void deleteUser(int id) {
-    	todoList.removeIf(todo-> todo.getId() == id);
+    	
+    	todoRepository.deleteById(id);
     }
 
 	public Todo getTodoById(int id) {
-		Todo todo1 = todoList.stream().filter(todo->todo.getId() == id).findFirst().get();
-		System.out.println("todo old data" + todo1);
-		return todo1;
+		return todoRepository.findById(id).get();
 	}
 
 	public void updateTodo(Todo newTodo) {
-		int index  = -1;
-		for(int i = 0; i < todoList.size(); i++) {
-			if(newTodo.getId() == todoList.get(i).getId()) {
-				index = i;
-				break;
-			}
-		}
-		System.out.println("newTodo"+newTodo);
-		if(index != -1)
-		todoList.set(index, newTodo);
-//		deleteUser(newTodo.getId());
-//		addToTodoList(username, newTodo.getDescription(), newTodo.getTarget(), newTodo.isDone());
+		todoRepository.save(newTodo);
 		
 	}
 
 	public List<Todo> getByUsername(String loggedInUser) {
-		System.out.println("getbyusername"+todoList);
-		List<Todo> todos = todoList.stream()
-				                   .filter(todo->todo.getUsername().equalsIgnoreCase(loggedInUser))
-				                   .toList();
-		return todos;
+		return todoRepository.findByUsername(loggedInUser);
+		
 	}
 }
